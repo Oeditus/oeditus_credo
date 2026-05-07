93cc798f2f9bb133b3826a1077a966690a8c9d80
Aleksei Matiushkin <am@ambment.cat>
2026-05-07 15:02:16 +0200

    :paperclip: version bump

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule OeditusCredo.MixProject do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
   @source_url "https://github.com/Oeditus/oeditus_credo"
   @homepage_url "https://oeditus.com"
 
